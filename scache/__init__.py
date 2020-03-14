35fc3ca3f92c913c215a54506527cbdf2f159fc5
Scott Blevins <sblevins@gmail.com>
2020-03-14 08:27:14 -0500

    Adding pandas support

--- scache/__init__.py
+++ scache/__init__.py
@@ -3,6 +3,11 @@ try:
 except ImportError:
     import json
 
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
+
 import os
 
 
@@ -13,44 +18,75 @@ class SCache():
         if not os.path.exists(path):
             os.makedirs(path)
 
-    def _get_path(self, key):
-        return '{}{}.json'.format(self.path, key)
+    def _get_path(self, key, ext='json'):
+        path = '{}{}.{}'.format(self.path, key, ext)
+        print(path)
+        return path
     
     def clear(self):
         for file_name in os.listdir(self.path):
-            if file_name.endswith('.json'):
+            if file_name.endswith('.json') or file_name.endswith('.csv'):
                 os.remove(self.path + file_name)
 
-    def rm(self, key):
-        path = self._get_path(key)
+    def rm(self, key, ext='json'):
+        path = self._get_path(key, ext=ext)
         if self.exists(key):
             os.remove(path)
         else:
             if self.debug:
                 print("Key: {} does not exist".format(key))
 
-    def exists(self, key):
-        path = self._get_path(key)
-        return os.path.exists(path)
+    def exists(self, key, ext='json'):
+        path = self._get_path(key, ext=ext)
+        exst = os.path.exists(path)
+        return exst
         
-    def set(self, key, value, pprint=True):
-        path = self._get_path(key)
-        with open(path, 'w+') as f:
-            if pprint:
-                f.write(json.dumps(value, sort_keys=True, indent=4, separators=(',', ': ')))
+    def set(self, key, value, pprint=True, as_type='json'):
+        if as_type == 'json':
+            path = self._get_path(key, ext='json')
+            if str(type(value)) == "<class 'pandas.core.frame.DataFrame'>":
+                value.to_json(path, orient='split')
             else:
-                f.write(json.dumps(value))
-
-
-    def get(self, key, fallback=dict()):
-        path = self._get_path(key)
-        value = fallback
-        if self.exists(key):
-            with open(path, 'r') as f:
-                _value = f.read()
-                value = json.loads(_value)
+                with open(path, 'w+') as f:
+                    if pprint:
+                        f.write(json.dumps(value, sort_keys=True, indent=4, separators=(',', ': ')))
+                    else:
+                        f.write(json.dumps(value))
+        elif as_type == 'csv':
+            path = self._get_path(key, ext='csv')
+            if str(type(value)) == "<class 'pandas.core.frame.DataFrame'>":
+                # Dump with pandas
+                value.to_csv(path, orient='split')
+            else:
+                print("Not implemented.")
         else:
-            if self.debug:
-                print("Couldn't get value.")
+            print("Unsupported file type.")
+
+
+    def get(self, key, fallback=dict(), ext='json', as_pandas=False):
+        path = self._get_path(key, ext=ext)
+        value = fallback
+        if as_pandas:
+            if pd:
+                if self.exists(key, ext=ext):
+                    if ext == 'json':
+                        value = pd.read_json(path)
+                    elif ext == 'csv':
+                        value = pd.read_csv(path)
+                    else:
+                        print("Not implemented.")
+                else:
+                    if self.debug:
+                        print("Couldn't get value.")
+            else:
+                print("Pandas is not installed.")
+        else:
+            if self.exists(key, ext=ext):
+                with open(path, 'r') as f:
+                    _value = f.read()
+                    value = json.loads(_value)
+            else:
+                if self.debug:
+                    print("Couldn't get value.")
 
         return value
